5f69a5ed46a1f8093e93650095653901a37a9ca6
Mersad <nickmersad81@gmail.com>
2024-10-27 08:37:48 +0330

    add const

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final primaryTextColor = const Color(0xff0D253C);
-    final secondryTextColor = const Color.fromARGB(255, 110, 127, 172);
+    const primaryTextColor = Color(0xff0D253C);
+    const secondryTextColor = Color.fromARGB(255, 110, 127, 172);
     return MaterialApp(
       title: 'Blog Club',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
           useMaterial3: true,
-          textTheme: TextTheme(
+          textTheme: const TextTheme(
               bodySmall: TextStyle(
                 color: secondryTextColor,
                 fontFamily: defaultFont,
